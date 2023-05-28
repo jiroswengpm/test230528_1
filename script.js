a342ef736b7be9afac535e2adbe7182880702110
jiroswengpm <125707800+jiroswengpm@users.noreply.github.com>
2023-05-28 20:20:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function splitText() {
   const length = document.getElementById('length').value;
   const text = document.getElementById('inputText').value;
   const resultDiv = document.getElementById('result');
+  const saveButton = document.getElementById('saveButton');
 
   if(length > 0) {
     let result = '';
@@ -13,11 +14,16 @@ function splitText() {
 
     // 文章をテキストファイルとして保存
     const blob = new Blob([result], {type: "text/plain;charset=utf-8"});
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
-    link.download = 'splitText.txt';
-    link.click();
-
+    const url = URL.createObjectURL(blob);
+    saveButton.style.display = 'block';
+    saveButton.onclick = function() {
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = 'splitText.txt';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
   } else {
     alert("分割する文字数を正しく入力してください。");
   }
